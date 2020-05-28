2c972ff8853720e18a76dc0388c3666ce8252c29
hezildsouza <63017211+hezildsouza@users.noreply.github.com>
2020-05-28 18:56:59 +0530

    Update edit.php

--- JavaScript-jQuery-and-JSON/Week1/edit.php
+++ JavaScript-jQuery-and-JSON/Week1/edit.php
@@ -1,166 +1,86 @@
 <?php
-
 session_start();
 
-if ( ! isset($_SESSION['name']) ) {
-    die("Not logged in");
+require_once "pdo.php";
+
+
+if (!isset($_SESSION['name'])) {
+    die('Not logged in');
 }
 
-// If the user requested logout go back to index.php
-if ( isset($_POST['cancel']) ) {
+
+if (isset($_POST['first_name']) && isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email'])
+    && isset($_POST['headline'])) {
+
+    if (strpos($_POST['email'], '@') === false) {
+        $_SESSION['error'] = 'Bad Email';
+    } else {
+
+        $sql = "UPDATE Profile SET first_name = :first_name, last_name = :last_name,email=:email,headline=:headline,summary=:summary
+            WHERE profile_id = :profile_id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array(
+                ':first_name' => $_POST['first_name'],
+                ':last_name' => $_POST['last_name'],
+                ':email' => $_POST['email'],
+                ':headline' => $_POST['headline'],
+                ':summary' => $_POST['summary'],
+                ':profile_id' => $_GET['profile_id'])
+        );
+        $_SESSION['success'] = 'Record updated';
+        header('Location: index.php');
+        return;
+    }
+}
+
+// Guardian: Make sure that user_id is present
+if (!isset($_GET['profile_id'])) {
+    $_SESSION['error'] = "Missing profile_id";
     header('Location: index.php');
     return;
 }
 
-$status = false;
-
-if ( isset($_SESSION['status']) ) {
-    $status = htmlentities($_SESSION['status']);
-    $status_color = htmlentities($_SESSION['color']);
-
-    unset($_SESSION['status']);
-    unset($_SESSION['color']);
+$stmt = $pdo->prepare("SELECT * FROM Profile where profile_id = :xyz");
+$stmt->execute(array(":xyz" => $_GET['profile_id']));
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+if ($row === false) {
+    $_SESSION['error'] = 'Bad value for user_id';
+    header('Location: index.php');
+    return;
 }
-
-try 
-{
-    $pdo = new PDO("mysql:host=localhost;dbname=coursera_javascript_jquery_and_json", "root", "root");
-    // set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e)
-{
-    echo "Connection failed: " . $e->getMessage();
-    die();
-}
-
-$name = htmlentities($_SESSION['name']);
-
-$_SESSION['color'] = 'red';
-
-if (isset($_REQUEST['profile_id']))
-{
-
-    $profile_id = htmlentities($_REQUEST['profile_id']);
-
-    // Check to see if we have some POST data, if we do process it
-    if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && isset($_POST['headline']) && isset($_POST['summary'])) 
-    {
-        if (strlen($_POST['first_name']) < 1 || strlen($_POST['last_name']) < 1 || strlen($_POST['email']) < 1 || strlen($_POST['headline']) < 1 || strlen($_POST['summary']) < 1)
-        {
-            $_SESSION['status'] = "All fields are required";
-            header("Location: edit.php?profile_id=" . htmlentities($_REQUEST['profile_id']));
-            return;
-        }
-
-        if (strpos($_POST['email'], '@') === false)
-        {
-            $_SESSION['status'] = "Email address must contain @";
-            header("Location: edit.php?profile_id=" . htmlentities($_REQUEST['profile_id']));
-            return;
-        }
-
-        $first_name = htmlentities($_POST['first_name']);
-        $last_name = htmlentities($_POST['last_name']);
-        $email = htmlentities($_POST['email']);
-        $headline = htmlentities($_POST['headline']);
-        $summary = htmlentities($_POST['summary']);
-
-        $stmt = $pdo->prepare("
-            UPDATE profile
-            SET first_name = :first_name, last_name = :last_name, email = :email, headline = :headline, summary = :summary
-            WHERE profile_id = :profile_id
-        ");
-
-        $stmt->execute([
-            ':first_name' => $first_name, 
-            ':last_name' => $last_name, 
-            ':email' => $email,
-            ':headline' => $headline,
-            ':summary' => $summary,
-            ':profile_id' => $profile_id,
-        ]);
-
-        $_SESSION['status'] = 'Record edited';
-        $_SESSION['color'] = 'green';
-
-        header('Location: index.php');
-        return;
-    }
-
-    $stmt = $pdo->prepare("
-        SELECT * FROM profile 
-        WHERE profile_id = :profile_id
-    ");
-
-    $stmt->execute([
-        ':profile_id' => $profile_id, 
-    ]);
-
-    $profile = $stmt->fetch(PDO::FETCH_OBJ);
-
-}
-
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Ivan Neradovic Autos</title>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    </head>
-    <body>
-        <div class="container">
-            <h1>Editing Profile for <?php echo $name; ?></h1>
-            <?php
-                if ( $status !== false ) 
-                {
-                    // Look closely at the use of single and double quotes
-                    echo(
-                        '<p style="color: ' .$status_color. ';" class="col-sm-10 col-sm-offset-2">'.
-                            htmlentities($status).
-                        "</p>\n"
-                    );
-                }
-            ?>
-            <form method="post" class="form-horizontal">
-                <div class="form-group">
-                    <label class="control-label col-sm-2" for="first_name">First Name:</label>
-                    <div class="col-sm-5">
-                        <input class="form-control" type="text" name="first_name" id="first_name" value="<?php echo $profile->first_name; ?>">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="control-label col-sm-2" for="last_name">Last Name:</label>
-                    <div class="col-sm-5">
-                        <input class="form-control" type="text" name="last_name" id="last_name" value="<?php echo $profile->last_name; ?>">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="control-label col-sm-2" for="email">Email:</label>
-                    <div class="col-sm-5">
-                        <input class="form-control" type="text" name="email" id="email" value="<?php echo $profile->email; ?>">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="control-label col-sm-2" for="headline">Headline:</label>
-                    <div class="col-sm-5">
-                        <input class="form-control" type="text" name="headline" id="headline" value="<?php echo $profile->headline; ?>">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="control-label col-sm-2" for="summary">Summary:</label>
-                    <div class="col-sm-5">
-                        <textarea class="form-control" name="summary" id="summary" rows="8"><?php echo $profile->summary; ?></textarea>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-sm-2 col-sm-offset-2">
-                        <input class="btn btn-primary" type="submit" value="Save">
-                        <input class="btn" type="submit" name="cancel" value="Cancel">
-                    </div>
-                </div>
-            </form>
-
-        </div>
-    </body>
+<head>
+    <?php require_once "bootstrap.php"; ?>
+    <title>Ziang Cui 's Login Page</title>
+</head>
+<body>
+<div class="container">
+    <h1>Editing Profile for UMSI</h1>
+    <?php
+    if (isset($_SESSION['error'])) {
+        echo('<p style="color: red;">' . htmlentities($_SESSION['error']) . "</p>\n");
+        unset($_SESSION['error']);
+    }
+    ?>
+    <form method="post">
+        <p>First Name:
+            <input type="text" name="first_name" size="60" value="<?php echo $row['first_name'] ?>"/></p>
+        <p>Last Name:
+            <input type="text" name="last_name" size="60" value="<?php echo $row['last_name'] ?>"/></p>
+        <p>Email:
+            <input type="text" name="email" size="30" value="<?php echo $row['email'] ?>"/></p>
+        <p>Headline:<br/>
+            <input type="text" name="headline" size="80" value="<?php echo $row['headline'] ?>"/></p>
+        <p>Summary:<br/>
+            <textarea name="summary" rows="8" cols="80"><?php echo $row['summary'] ?></textarea>
+        <p>
+            <input type="submit" value="Save">
+            <input type="submit" name="cancel" value="Cancel">
+        </p>
+    </form>
+    <p>
+</div>
+</body>
 </html>
