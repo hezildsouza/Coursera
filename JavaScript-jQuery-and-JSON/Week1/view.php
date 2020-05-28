8d6fd8293f5f92b3c9b57331d96ce2ef9de9535b
hezildsouza <63017211+hezildsouza@users.noreply.github.com>
2020-05-28 19:06:07 +0530

    Update view.php

--- JavaScript-jQuery-and-JSON/Week1/view.php
+++ JavaScript-jQuery-and-JSON/Week1/view.php
@@ -1,88 +1,32 @@
-<?php
-
+<?php // Do not put any HTML above this line
 session_start();
+require_once "pdo.php";
 
-if ( ! isset($_SESSION['name']) ) {
-    die("Not logged in");
+if (!isset($_GET['profile_id'])) {
+    $_SESSION['error'] = "Missing autos_id";
+    header('Location: index.php');
+    return;
 }
 
-try 
-{
-    $pdo = new PDO("mysql:host=localhost;dbname=coursera_javascript_jquery_and_json", "root", "root");
-    // set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e)
-{
-    echo "Connection failed: " . $e->getMessage();
-    die();
-}
-
-if (isset($_REQUEST['profile_id']))
-{
-    $profile_id = htmlentities($_REQUEST['profile_id']);
-
-    $stmt = $pdo->prepare("
-        SELECT * FROM profile 
-        WHERE profile_id = :profile_id
-    ");
-
-    $stmt->execute([
-        ':profile_id' => $profile_id, 
-    ]);
-
-    $profile = $stmt->fetch(PDO::FETCH_OBJ);
-}
-
+$stmt = $pdo->prepare("SELECT * FROM Profile where profile_id = :xyz");
+$stmt->execute(array(":xyz" => $_GET['profile_id']));
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Ivan Neradovic Autos</title>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-        <style type="text/css">
-            .row {padding: 5px 0;}
-        </style>
-    </head>
-    <body>
-        <div class="container">
-
-            <h1>Profile information</h1>
-
-            <div class="row">
-                <div class="col-sm-2">First Name:</div>
-                <div class="col-sm-4">
-                    <?php echo $profile->first_name; ?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-2">Last Name:</div>
-                <div class="col-sm-4">
-                    <?php echo $profile->last_name; ?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-2">Email:</div>
-                <div class="col-sm-4">
-                    <?php echo $profile->email; ?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-2">Headline:</div>
-                <div class="col-sm-4">
-                    <?php echo $profile->headline; ?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-2">Last Name:</div>
-                <div class="col-sm-8">
-                    <?php echo $profile->summary; ?>
-                </div>
-            </div>
-
-            <p><a href="index.php">Done</a></p>
-
-        </div>
-    </body>
+<head>
+    <?php require_once "bootstrap.php"; ?>
+    <title>Ziang Cui 's Login Page</title>
+</head>
+<body>
+<div class="container">
+    <h1>Profile information</h1>
+    <p>First Name: <?php echo($row['first_name']); ?></p>
+    <p>Last Name: <?php echo($row['last_name']); ?></p>
+    <p>Email: <?php echo($row['email']); ?></p>
+    <p>Headline:<br/> <?php echo($row['headline']); ?></p>
+    <p>Summary: <br/><?php echo($row['summary']); ?></p>
+    <a href="index.php">Done</a>
+</div>
+</body>
 </html>
